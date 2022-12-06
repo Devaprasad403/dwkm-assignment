d97729dcce1f44308e780fc8467b3d5d62c07c57
Devaprasad403 <95910633+Devaprasad403@users.noreply.github.com>
2022-12-06 13:28:23 +0530

    feature : add code for google stock viz

---
+++ module 2/stock-viz/code.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import yfinance
+import matplotlib.pyplot as plt
+from scipy.stats import skew,kurtosis,norm,skewtest,kurtosistest
+from statsmodels.graphics.tsaplots import plot_pacf,plot_acf
+
+name = 'GOOG'
+ticker = yfinance.Ticker(name)
+df = ticker.history(interval="1d",start="2015-03-15",end="2020-09-10")
+x = df['Close']
+
+returns = x.pct_change(1).dropna()
+
+plt.hist(returns,bins="rice",label="Daily close price")
+plt.legend()
+plt.show()
+
+plt.boxplot(returns,labels=["Daily close price"])
+plt.show()
+
+t = np.linspace(0.01,0.99,1000)
+q1 = np.quantile(returns,t)
+q2 = norm.ppf(t,loc=np.mean(returns),scale=np.std(returns))
+plt.plot(q1,q2)
+plt.plot([min(q1),max(q1)],[min(q2),max(q2)])
+plt.xlim((min(q1),max(q1)))
+plt.ylim((min(q2),max(q2)))
+plt.xlabel("Daily returns")
+plt.ylabel("Normal distribution")
+plt.show()
+
+plt.plot(returns)
+plt.xlabel("Time")
+plt.ylabel("Daily returns")
+plt.show()
+
+plt.plot(returns.rolling(20).std())
+plt.xlabel("Time")
+plt.ylabel("20-days rolling standard deviation")
+plt.show()
+
+plot_pacf(returns,lags=20)
+
+
+
